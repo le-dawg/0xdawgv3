b4ee56c9f799e151ab7c988f94ecafb7de277a8e
Stackbit Code Editor <projects@stackbit.com>
2021-07-18 22:12:17 +0000

    Calendly.js: changed

--- src/components/Calendly.js
+++ src/components/Calendly.js
@@ -3,15 +3,27 @@ import _ from 'lodash';
 
 import Action from './Action';
 
-export default class CtaButtons extends React.Component {
-    render() {
-        let actions = _.get(this.props, 'actions', null);
-        return (
-            _.map(actions, (action, action_idx) => (
-              <Action key={action_idx} {...this.props} action={action} />
-            ))
-        );
-    }
+export default class Calendly extends Component {
+  componentDidMount() {
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
+    script.setAttribute('src',  'https://assets.calendly.com/assets      
+                        /external/widget.js');
+    head.appendChild(script);
+  }  componentWillUnmount() {
+    // whatever you need to cleanup the widgets code
+  }  render(){
+    return (
+      <div>
+        <div id="schedule_form">
+          <div 
+            className="calendly-inline-widget"
+            data-url="https://calendly.com/username/15min"
+            style={{ minWidth: '320px', height: '580px' }} />
+        </div>
+      </div>
+    );
+  }
 }
 
 class Calendly extends Component {
