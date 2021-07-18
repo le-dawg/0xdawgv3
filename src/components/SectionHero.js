bae37d5fe757c17bbf942244c528e40081cec862
Stackbit Code Editor <projects@stackbit.com>
2021-07-18 22:26:03 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -25,8 +25,15 @@ export default class SectionHero extends React.Component {
                   <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
                 </div>
                 )}
-                )}
               </div>
+              <div>
+        <div id="schedule_form">
+          <div 
+            className="calendly-inline-widget"
+            data-url="https://calendly.com/dawg"
+            style={{ minWidth: '320px', height: '580px' }} />
+        </div>
+      </div>
             </section>
         );
     }
