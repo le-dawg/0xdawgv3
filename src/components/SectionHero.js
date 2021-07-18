69bb8a6377b63ed6e262ba38e020a3bfce4782a0
Stackbit Code Editor <projects@stackbit.com>
2021-07-18 21:58:23 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -9,7 +9,7 @@ export default class SectionHero extends React.Component {
         let section = _.get(this.props, 'section', null);
         return (
             <section id={_.get(section, 'section_id', null)} className="block block-hero outer">
-              <div className="inner-contact">
+              <div className="inner">
                 {_.get(section, 'title', null) && (
                 <div className="block-header inner-sm">
                   <h1 className="block-title">{_.get(section, 'title', null)}</h1>
