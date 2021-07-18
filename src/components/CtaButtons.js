ac9b1ff3e776a251efb27e224c09e7899087489e
Stackbit Code Editor <projects@stackbit.com>
2021-07-18 22:13:04 +0000

    CtaButtons.js: changed

--- src/components/CtaButtons.js
+++ src/components/CtaButtons.js
@@ -1,37 +1,15 @@
-import React from 'react'
-import {InlineWidget} from 'react-calendly'class Calendly extends Component {
-  componentDidMount() {
-    // whatever stuff you need here
-  }  componentWillUnmount() {
-    // whatever cleanup stuff you need here
-  }  render(){
-    return (
-      <div>
-        <InlineWidget url="https://calendly.com/username/15min" />
-      </div>
-    );
-  }
-}
+import React from 'react';
+import _ from 'lodash';
 
-class Calendly extends Component {
-  componentDidMount() {
-    const head = document.querySelector('head');
-    const script = document.createElement('script');
-    script.setAttribute('src',  'https://assets.calendly.com/assets      
-                        /external/widget.js');
-    head.appendChild(script);
-  }  componentWillUnmount() {
-    // whatever you need to cleanup the widgets code
-  }  render(){
-    return (
-      <div>
-        <div id="schedule_form">
-          <div 
-            className="calendly-inline-widget"
-            data-url="https://calendly.com/username/15min"
-            style={{ minWidth: '320px', height: '580px' }} />
-        </div>
-      </div>
-    );
-  }
+import Action from './Action';
+
+export default class CtaButtons extends React.Component {
+    render() {
+        let actions = _.get(this.props, 'actions', null);
+        return (
+            _.map(actions, (action, action_idx) => (
+              <Action key={action_idx} {...this.props} action={action} />
+            ))
+        );
+    }
 }
